7d50045b080be0a4757d50719f27726698014cd3
lucia8716 <crespopopo@gmail.com>
2021-01-20 13:12:12 +0100

    v1

--- parking/parking.js
+++ parking/parking.js
@@ -1,15 +1,56 @@
 var filnom=/^(?!.* (?: |$))[a-z\-]+$/;
+var filtercontraseña=/^([a-zA-Z0-9]){6}/;
+var filtermatricula=/^([0-9]){4}([a-zA-Z]){3}/;
 
 function loging(){
 
 x1=$('#i1').val();
+x2=$('#i2').val();
 
 if(filnom.test(x1)==false){
 
-    toastr.warning("mal")
+    toastr.warning("Nombre con formato incorrecto");
     $('#i1').val('');
     $('#i1').focus();
     
 }
 
+if(filtercontraseña.test(x2)==false){
+
+    toastr.warning("Contraseña con formato incorrecto");
+    $('#i2').val('');
+    $('#i2').focus();
+    
 }
+
+}
+function entrada() { //muestra y quita div
+
+    $("#di2").hide();
+    $("#di3").fadeIn("slow");
+  }
+
+function matriculacion(){
+
+    x3=$('#i3').val();
+    x3=x3.toUpperCase();
+console.log(x3);
+
+    if(filtermatricula.test(x3)==false){
+
+        toastr.warning("Matricula con formato incorrecto");
+        $('#i3').val('');
+        $('#i3').focus();
+        
+    }
+
+  }
+
+
+ function volver3(){//funcion para el boton de volver
+    $("#di3").hide();
+      $("#di2").fadeIn("slow");
+      document.getElementById('i3').value='';
+  
+  
+  }
--- parking/parking.php
+++ parking/parking.php
@@ -52,10 +52,41 @@
 
     </div>
 
+    <div class="dv" id="di3">
+        <img id="ima1" src="1.png">
+
+        <form method="POST" action="">
+            <p>
+                <input id="i3" type="text" name="matricula" required placeholder="Numero de matricula" maxlength="7" minlength="7">
+                <button id="button5" type="submit" name="boton5" onclick="matriculacion()">Comprobar</button>
+                <button id="button8" onclick="volver3()">Salir</button>
+            </p>
+
+        </form>
+    </div>
+
+
+    <div class="dv" id="di4">
+        <img id="ima1" src="1.png">
+
+        <p>
+
+            <input id="i4" type="text" name="matricula" required placeholder="Numero de matricula" maxlength="7" minlength="7" readonly>
+            <input id="i5" type="text" name="marca" required placeholder="Marca">
+            <input id="i6" type="text" name="modelo" required placeholder="Modelo">
+            <input id="i11" type="text" name="color" required placeholder="Color">
+            <input id="i7" type="text" name="nombre" required placeholder="Nombre Propietario">
+            <input id="i8" type="text" name="ape1" required placeholder="Apellido 1 Propietario">
+            <input id="i9" type="text" name="ape2" required placeholder="Apellido 2 Propietario">
+            <input id="i10" type="text" name="email" required placeholder="Email">
+
+            <button id="button9" onclick="altanueva()">Registrar</button>
+
+        </p>
+    </div>
 
 
     <?php
-
     //echo '<script type="text/javascript">$("#di2").hide();</script>';
     if (isset($_POST['boton1'])) {
         $nombre = $_POST['nombre'];
@@ -84,8 +115,36 @@
             echo '<script type="text/javascript">toastr.warning("Usuario no registrado")</script>';
         }
         mysqli_close($conexion);
+    } elseif (isset($_POST['boton5'])) {
+
+
+
+        $matricula = $_POST['matricula'];
+        require_once "conectar_bd.php";
+
+        $controldematricula = mysqli_query($conexion, 'SELECT * from coches where matricula ="' . $matricula . '"') or die("Error en la consulta");
+
+        if (mysqli_num_rows($controldematricula) > 0) {
+
+
+            echo '<script type="text/javascript">
+            toastr.options = {"positionClass": "toast-top-center","timeOut": "1200",}
+            toastr.success("coche registrado")</script>';
+        } else {
+            echo 'coche no registrado';
+        }
+
+        mysqli_close($conexion);
     }
+
+
     ?>
+
+
+
+
+
+
 
 
 </body>
