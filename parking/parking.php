4125975f1723a880ad147ad6ebdca986013af194
lucia8716 <crespopopo@gmail.com>
2021-01-20 10:29:51 +0100

    v1

--- parking/parking.php
+++ parking/parking.php
@@ -32,7 +32,7 @@
         <p>
         <form method="POST" action="">
             <input id="i1" type="text" name="nombre" required placeholder="Nombre">
-            <input id="i2" type="password" name="clave">
+            <input id="i2" type="password" name="clave" required placeholder="Contraseña" minlength="6" maxlength="6">
             <button id="button1" type="submit" name="boton1" onclick="loging()"><img id="ima2" src="ope.jpg"></button>
         </form>
         </p>
@@ -59,21 +59,29 @@
     //echo '<script type="text/javascript">$("#di2").hide();</script>';
     if (isset($_POST['boton1'])) {
         $nombre = $_POST['nombre'];
-        $contraseña = intval($_POST['clave']);
+        $contrasena = $_POST['clave'];
         require_once "conectar_bd.php";
-        $controlusuario = 'SELECT * from nombre where nombre ="' . $nombre . '" and contraseña="' . $contraseña . '"';
-        $resultado = mysqli_query($conexion, $controlusuario);
-        if (mysqli_num_rows($resultado) > 0) { // La variable $conexion proviene del archivo conectar_bd.php
+        $controlusuario = 'SELECT * from usuarios where nombre ="' . $nombre . '" and contrasena =' . $contrasena . '';
+        $resultado = mysqli_query($conexion, $controlusuario) or die("Problemas en el select" . mysqli_error($conexion));
 
-            echo '<script type="text/javascript">
+        $controldenombre = mysqli_query($conexion, 'SELECT * from usuarios where nombre ="' . $nombre . '"') or die("Error en la consulta");
+
+
+        if (mysqli_num_rows($controldenombre) > 0) {
+            if (mysqli_num_rows($resultado) > 0) { // La variable $conexion proviene del archivo conectar_bd.php
+
+                echo '<script type="text/javascript">
             toastr.options = {"positionClass": "toast-top-center","timeOut": "1200",}
-            toastr.success("Cargando pagina")
+            toastr.success("Usuario conectado, Cargando pagina")
             document.getElementById("di1").setAttribute("style","display:none;");
             document.getElementById("di2").setAttribute("style","display:block;");
                  
             </script>';
+            } else {
+                echo '<script type="text/javascript">toastr.warning("Contraseña incorrecta")</script>';
+            }
         } else {
-            echo '<script type="text/javascript">toastr.warning("Usuario no regitrado")</script>';
+            echo '<script type="text/javascript">toastr.warning("Usuario no registrado")</script>';
         }
         mysqli_close($conexion);
     }
