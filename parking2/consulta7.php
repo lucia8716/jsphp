d50a4512fa6237c6381156cb69cdd789752fca1b
Lucia8716 <crespopopo@gmail.com>
2021-01-23 03:06:09 +0100

    v1

--- parking2/consulta7.php
+++ parking2/consulta7.php
@@ -3,7 +3,27 @@
 require_once "conectar_bd.php";
 $actualizacionestancia = 'select * from coches';
 $busqueda = mysqli_query($conexion, $actualizacionestancia) or die("Problemas en el select" . mysqli_error($conexion));
-$arrayresultados = mysqli_fetch_assoc($busqueda);
-$json = json_encode($arrayresultados, JSON_UNESCAPED_UNICODE);
+
+$htmltable = '';
+
+while ($arrayresultados = mysqli_fetch_assoc($busqueda)) {
+
+    $htmltable .= '<table id="tabla2">
+    <thead id="mostrar">
+        <tr>
+            <th>' . $arrayresultados['matricula'] . '</th>
+            <th>' . $arrayresultados['marca'] . '</th>
+            <th>' . $arrayresultados['modelo'] . '</th>
+            <th>' . $arrayresultados['color'] . '</th>
+            <th>' . $arrayresultados['nombre'] . '</th>
+            <th>' . $arrayresultados['apellido_1'] . '</th>
+            <th>' . $arrayresultados['apellido_2'] . '</th>
+            <th>' . $arrayresultados['email'] . '</th>
+            <th>' . $arrayresultados['estancia'] . '</th>
+        </tr>
+    </thead>
+</table>';
+};
+$json = json_encode($htmltable, JSON_UNESCAPED_UNICODE);
 echo $json;
 mysqli_close($conexion);
